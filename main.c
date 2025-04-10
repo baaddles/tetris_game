9131c929dca99aea6a5db704cf2667e278d7264e
Ayato7463 <adrien.co82@gmail.com>
2025-04-10 17:19:13 +0200

    Ajout encore d'un printf

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 int main(int, char**){
     printf("Hello, from tetrisproject!\n");
     printf("Modif Test !!\n");
+    printf("Encore, ca fait beaucoup la non ?");
 
     printf("Test encore.\n");
     printf("\n");
