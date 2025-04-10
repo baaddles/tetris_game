d944ac834b7ea09a533c601d8b7b4377d01271fe
Ayato7463 <adrien.co82@gmail.com>
2025-04-10 17:32:21 +0200

    zizi solo

--- main.c
+++ main.c
@@ -4,8 +4,13 @@ int main(int, char**){
     printf("Hello, from tetrisproject!\n");
     printf("Modif Test !!\n");
     printf("Encore, ca fait beaucoup la non ?");
+    printf("jai un gros zizi.\n");
 
-    printf("jai un gros paff Test encore.\n");
+    for (int k = 0 ; k < 3 ; k++){
+        printf("Caca numéro %d.\n",k);
+    }
+
+    printf("Test encore.\n");
     printf("\n");
 
 
