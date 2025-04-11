cdda43b429509d8353cd5186273dc1d59e7192c2
baaddles <leo.gnrpro@gmail.com>
2025-04-11 15:04:05 +0200

    Remise du graph

--- src/Tetris/main.c
+++ src/Tetris/main.c
@@ -1,7 +1,35 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <ncurses.h>
 
-#include "defines.h"
+int main(int argc, char**argv){
+    printf("========== Start ==========\n");
 
-int main(){
-    printf("tetris board height : %d\n", TETRIS_BOARD_HEIGHT);
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    timeout(100);
+    curs_set(0);
+
+    box(stdscr, 0, 0);
+
+    mvwprintw(stdscr, 1, 1, "Appuyer sur q pour quitter le programme...");
+
+    int ch;
+    do{
+        ch = getch();
+        switch(ch){
+            case 'q':
+            break;
+            default:
+            break;
+        }
+    }while(ch != 'q');
+
+    wclear(stdscr);
+    endwin();
+
+    printf("========== End ==========\n");
+    return EXIT_SUCCESS;
 }
